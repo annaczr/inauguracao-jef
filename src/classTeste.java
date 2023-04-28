b8699348fec38d44597864be92623907565ef1b9
annaczr <annaalufc231106@gmail.com>
2023-04-28 16:41:04 -0300

    adicionando testes

---
+++ src/classTeste.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class classTeste {
+    public static void main (String[] args) {
+        ArrayList<Colaborador> colabs= new ArrayList<Colaborador>();
+
+        colabs.add( new Colaborador("Anna","1234567890","12-03-2004","30-04-2023","m"));
+        colabs.add( new Colaborador("Anna2","1234567890","12-03-2004","30-04-2023","m"));
+        colabs.add( new Colaborador("Anna3","1234567890","12-03-2004","30-04-2023","m"));
+    }
+}
